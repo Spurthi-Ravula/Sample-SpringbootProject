38df145146fc94d3dc9d7c476d402124baedba69
Spurthi-Ravula <spurthiravula26@gmail.com>
2023-11-11 21:19:52 -0600

    My first commit

---
+++ src/main/java/com/example/DogManagementSystem/DogManagementSystemApplication.java
@@ -0,0 +1,16 @@
+package com.example.DogManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Spurthi Ravula S559190
+ */
+@SpringBootApplication
+public class DogManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogManagementSystemApplication.class, args);
+	}
+
+}
